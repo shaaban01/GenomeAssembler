640373625dcef5ae7d7e638d3f1a76bb7222108d
shaaban01 <mmc53@mail.aub.edu>
2023-11-13 20:24:46 +0200

    added the eulerian path

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,27 @@
 #include <iostream>
+#include "../include/kmerifier.h"
+#include "../include/graph.h"
 
-int main(int argc, char **argv)
+int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    KMerifier kmerifier(4);
+    std::vector<std::string> reads = {"AAGATTCTCTAAGA"};
+
+    kmerifier.TransformReadsToKmers(reads);
+    DeBruijnGraph graph(kmerifier.GetKmers());
+    graph.printGraph();
+    std::cout << "Start node: " << graph.FindStartNode() << std::endl;
+    std::cout << "Eulerian path: ";
+    for (const auto &node : graph.FindEulerianPath())
+    {
+        std::cout << node << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Is valid Eulerian path: " << graph.IsValidEulerianPath() << std::endl;
+
+    for (const auto node : graph.GenerateContigs(graph.FindEulerianPath(), 4))
+    {
+        std::cout << node << std::endl;
+    }
     return 0;
 }
-
-// Just to have an Idea of how this code be implemented -- THIS IS NOT THE FINAL CODE
-
-// #include <iostream>
-// #include "preprocessing.h"
-// #include "graph.h"
-// #include "contig.h"
-
-// int main() {
-// Step 1: Read and preprocess input data
-//     std::string inputFileName = "input.fasta";  // Replace with your actual input file
-//     std::string outputFileName = "output.fasta";  // Replace with your desired output file
-
-//     Preprocessing preprocessing;
-//     if (preprocessing.loadInputData(inputFileName)) {
-//         preprocessing.performQualityControl();
-//         preprocessing.performErrorCorrection();
-//         preprocessing.saveProcessedData("preprocessed_data.fasta");
-//     } else {
-//         std::cerr << "Error loading input data." << std::endl;
-//         return 1;  // Exit with an error code
-//     }
-
-// Step 2: Construct the De Bruijn graph
-//     DeBruijnGraph deBruijnGraph;
-//     deBruijnGraph.constructGraph(preprocessing.getProcessedData());
-
-// Step 3: Simplify the graph
-//     deBruijnGraph.simplifyGraph(); -- this might include the eularin path algorithm or other algorithms
-
-// Step 4: Generate contigs
-//     ContigGeneration contigGeneration;
-//     contigGeneration.generateContigs(deBruijnGraph);
-
-// Step 5: Save assembled contigs to the output file
-//     contigGeneration.saveContigs(outputFileName);
-
-//     std::cout << "Assembly completed successfully." << std::endl;
-//     return 0;  // Exit with a success code
-// }
