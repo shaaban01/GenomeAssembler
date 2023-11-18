6a5c66f4bc0a843df5508b455315f2e0313367f3
shaaban01 <mmc53@mail.aub.edu>
2023-11-18 17:19:31 +0200

    merged the preproc

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,37 @@
 #include <chrono> // include chrono library
 #include "../include/kmerifier.h"
 #include "../include/graph.h"
+#include "../include/preprocessing.h"
 
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now(); // start timer
-    KMerifier kmerifier(5);
-    std::vector<std::string> reads = {"AAGATTCTCTAAGA"};
-    kmerifier.TransformReadsToKmers(reads);
+    std::string filename = "../data/reads.txt";
+
+    // Read genome reads from the file
+    auto startReading = std::chrono::high_resolution_clock::now(); // start timer for file reading
+    auto result = readGenomeReads(filename);
+    auto endReading = std::chrono::high_resolution_clock::now(); // end timer for file reading
+
+    // Access the vector of genome reads and the length of each read
+    std::vector<std::string> genomeReads = result.first;
+    int k = result.second;
+
+    // Process the genome reads
+    std::cout << "Done reading\n";
+
+    auto start = std::chrono::high_resolution_clock::now(); // start timer for processing
+    KMerifier kmerifier(k);
+    std::cout << k << "-merifier created \n";
+    kmerifier.TransformReadsToKmers(genomeReads);
     DeBruijnGraph graph(kmerifier);
-    graph.printGraph();
-    std::cout << "Eulerian path: " << graph.DoEulerianWalk() << std::endl;
-    auto end = std::chrono::high_resolution_clock::now();                                        // end timer
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);          // calculate duration in microseconds
-    std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl; // print duration in microseconds
+    std::cout << "Eulerian path: " << graph.DoEulerianWalk() << "\n";
+    auto end = std::chrono::high_resolution_clock::now(); // end timer for processing
+
+    auto durationReading = std::chrono::duration_cast<std::chrono::microseconds>(endReading - startReading); // calculate duration for file reading in microseconds
+    auto durationProcessing = std::chrono::duration_cast<std::chrono::microseconds>(end - start);            // calculate duration for processing in microseconds
+
+    std::cout << "Time taken for file reading: " << durationReading.count() << " microseconds\n";  // print duration for file reading in microseconds
+    std::cout << "Time taken for processing: " << durationProcessing.count() << " microseconds\n"; // print duration for processing in microseconds
 
     return 0;
 }
