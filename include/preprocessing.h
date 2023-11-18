3ecf6b9208b26c6d40c172897ce6dbdf0069d8ee
shaaban01 <mmc53@mail.aub.edu>
2023-11-18 17:19:31 +0200

    merged the preproc

--- include/preprocessing.h
+++ include/preprocessing.h
@@ -1,11 +1,10 @@
-// Declare functions and classes for data preprocessing, including quality control and error correction(maybe later).
-#ifndef GENOME_READER_H
-#define GENOME_READER_H
+#pragma once
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-std::pair<std::vector<std::string>, int> readGenomeReads(const std::string& filename);
-void processGenomeReads(const std::vector<std::string>& genomeReads, int k);
-
-#endif  // GENOME_READER_H
+std::pair<std::vector<std::string>, int> readGenomeReads(const std::string &filename);
+void processGenomeReads(const std::vector<std::string> &genomeReads, int k);
--- src/preproccessing.cpp
+++ src/preproccessing.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
 #include "../include/preprocessing.h"
 
-std::pair<std::vector<std::string>, int> readGenomeReads(const std::string& filename) {
+std::pair<std::vector<std::string>, int> readGenomeReads(const std::string &filename)
+{
     std::ifstream inputFile(filename);
 
     // Check if the file is opened successfully
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         std::cerr << "Error opening the file." << std::endl;
         return {{}, 0}; // Return an empty vector and 0 length
     }
 
     // Read the first genome read from the file
     std::string firstGenomeRead;
-    if (std::getline(inputFile, firstGenomeRead)) {
+    if (std::getline(inputFile, firstGenomeRead))
+    {
         // Get the length of the genome from the first read
         const int genomeLength = firstGenomeRead.length();
 
@@ -29,12 +28,16 @@ std::pair<std::vector<std::string>, int> readGenomeReads(const std::string& file
         // Read and store the rest of the genome reads
         std::vector<std::string> genomeReads;
         std::string genomeRead;
-        while (std::getline(inputFile, genomeRead)) {
+        while (std::getline(inputFile, genomeRead))
+        {
             // Check if the read has the expected length
-            if (genomeRead.length() == genomeLength) {
+            if (genomeRead.length() == genomeLength)
+            {
                 // Store the genome read in the vector
                 genomeReads.push_back(genomeRead);
-            } else {
+            }
+            else
+            {
                 std::cerr << "Error: Genome read does not have the expected length." << std::endl;
                 // You might want to handle this error case differently based on your requirements
             }
@@ -45,7 +48,9 @@ std::pair<std::vector<std::string>, int> readGenomeReads(const std::string& file
 
         // Return the vector of genome reads and the length of each read
         return {genomeReads, genomeLength};
-    } else {
+    }
+    else
+    {
         std::cerr << "Error: Unable to read the first genome read." << std::endl;
         // Close the file
         inputFile.close();
@@ -53,28 +58,13 @@ std::pair<std::vector<std::string>, int> readGenomeReads(const std::string& file
     }
 }
 
-void processGenomeReads(const std::vector<std::string>& genomeReads, int k) {
+void processGenomeReads(const std::vector<std::string> &genomeReads, int k)
+{
     // Process or use the genome reads as needed
-    for (const auto& read : genomeReads) {
+    for (const auto &read : genomeReads)
+    {
         std::cout << "Genome Read: " << read << std::endl;
     }
 
     std::cout << "Length of each read (k): " << k << std::endl;
-}
-
-int main() {
-    // Specify the filename
-    std::string filename = "../files/reads.txt";
-
-    // Read genome reads from the file
-    auto result = readGenomeReads(filename);
-
-    // Access the vector of genome reads and the length of each read
-    std::vector<std::string> genomeReads = result.first;
-    int k = result.second;
-
-    // Process the genome reads
-    processGenomeReads(genomeReads, k);
-    std::cout << "Done! " << std::endl;
-    return 0; // Return success code
 }
